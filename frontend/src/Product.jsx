e44b92f13a84eed688a53bf5f28afb0bf1d82939
github.com/robotsson <game-baked-jogging@duck.com>
2025-01-13 19:39:31 +0100

    props and product component

---
+++ frontend/src/Product.jsx
@@ -0,0 +1,9 @@
+
+function Product({name}) 
+{
+  return (
+    <div className="rounded-lg shadow-md bg-blue-100 h-40">{name}</div>
+  )
+}
+
+export default Product;
--- frontend/src/Products.jsx
+++ frontend/src/Products.jsx
@@ -1,3 +1,4 @@
+import Product from './Product';
 
 function Products() 
 {
@@ -7,22 +8,25 @@ function Products()
     <div className="flex flex-col flex-grow bg-green-100 shadow-md rounded-lg">
       
       <div class="grid grid-cols-5 mt-3 gap-4">
-        <div className="rounded-lg shadow-md bg-blue-100 h-40">01 Hamburger</div>
-        <div className="rounded-lg shadow-md bg-blue-100 ">02 Cheeseburger</div>
-        <div className="rounded-lg shadow-md bg-blue-100">03 Dbl Cheese</div>        
-        <div className="rounded-lg shadow-md bg-blue-100">04 Chicken burger</div>
-        <div className="rounded-lg shadow-md bg-blue-100">05 Veg burger</div>
-        <div className="rounded-lg shadow-md bg-blue-100 h-40">06 French Fries</div>
-        <div className="rounded-lg shadow-md bg-blue-100">07 Cheese Fries</div>
-        <div className="rounded-lg shadow-md bg-blue-100">08 Mayo</div>
-        <div className="rounded-lg shadow-md bg-blue-100">09 Garlic</div>
-        <div className="rounded-lg shadow-md bg-blue-100">10 Guacamole</div>
-        <div className="rounded-lg shadow-md bg-blue-100 h-40">11 Coca-Cola</div>
-        <div className="rounded-lg shadow-md bg-blue-100">12 Coke Zero</div>
-        <div className="rounded-lg shadow-md bg-blue-100">13 Fanta</div>
-        <div className="rounded-lg shadow-md bg-blue-100">14 Sprite</div>
-        <div className="rounded-lg shadow-md bg-blue-100">15 Dr Pepper</div>
+        <Product name={"Hamburger"}/>
+        <Product name={"Cheeseburger"}/>
+        <Product name={"Dbl Cheese"}/>
+        <Product name={"Chicken burger"}/>
+        <Product name={"Veg burger"}/>
+
+        <Product name={"French Fries"}/>
+        <Product name={"Cheese Fries"}/>
+        <Product name={"Mayo dip"}/>
+        <Product name={"Garlic dip"}/>
+        <Product name={"Guacamole"}/>
+
+        <Product name={"Coca-cola"}/>
+        <Product name={"Coke zero"}/>
+        <Product name={"Fanta"}/>
+        <Product name={"Sprite"}/>
+        <Product name={"Dr Pepper"}/>      
       </div>
+      
     </div>
   )
 }
