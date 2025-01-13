fe7069f64c8b51c2f12107bf3e26c0aa81727b1a
github.com/robotsson <game-baked-jogging@duck.com>
2025-01-13 01:36:53 +0100

    fronted beginnings

---
+++ frontend/src/OrderConfirmed.jsx
@@ -0,0 +1,18 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './OrderConfirmed.css'
+
+
+function OrderConfirmed() 
+{
+
+  return (
+    <>
+    <div className="flex justify-evenly bg-blue-100 items-end">
+        Order sent.
+    </div>
+    </>
+  )
+}
+
+export default OrderConfirmed;
---
+++ frontend/src/OrderPage.jsx
@@ -0,0 +1,20 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './OrderPage.css'
+
+
+function OrderPage() 
+{
+
+  return (
+    <>
+      <div className="flex justify-evenly bg-green-100 items-end shadow-md rounded-lg">
+        <h1>&#127828;</h1>
+        <span className="text-sm">products here</span>
+        <h1>&#127828;</h1>
+      </div>
+    </>
+  )
+}
+
+export default OrderPage;
